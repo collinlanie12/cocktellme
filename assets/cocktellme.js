5708bc2083dbb6c019a33e80f422e7dacff3b970
ksimone <ksworthy@windstream.net>
2020-08-17 14:26:41 -0400

    updating

--- assets/cocktellme.js
+++ assets/cocktellme.js
@@ -7,4 +7,17 @@ $.ajax({
   }).then(function(response) {
     //console.log(cocktailURL);
     console.log(response);
+
+    $('.uk-button').on("click", function(){
+      window.location='./questions.html';
+    });
+
+    //$("#new-img").attr("src", response.drinks[1].strDrinkThumb);
+    for(var i = 0; i < response.drinks.length; i++){
+      var newLi = $("<li>");
+      var newImg = $("<img>");
+      newImg.attr("src", response.drinks[i].strDrinkThumb);
+      newLi.append(newImg);
+      //$(".uk-slider-items").append(newLi);
+    }
   });
