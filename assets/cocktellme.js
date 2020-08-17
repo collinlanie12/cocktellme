bc5aa07f4346dc0d0d8816bc9564f008e378759f
ksimone <ksworthy@windstream.net>
2020-08-17 11:38:08 -0400

    new changes

--- assets/cocktellme.js
+++ assets/cocktellme.js
@@ -1,14 +1,10 @@
+cocktailURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic"
 
-
-cocktailURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a"
-
-
-console.log(cocktailURL);
 
 $.ajax({
     url: cocktailURL,
     method: "GET"
   }).then(function(response) {
-    console.log(cocktailURL);
+    //console.log(cocktailURL);
     console.log(response);
   });
