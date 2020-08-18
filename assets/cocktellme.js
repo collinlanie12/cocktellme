03bd421334dacfb85ceb5f27885071b7278851b7
Collin Lanier <collinlanier12@gmail.com>
2020-08-18 15:36:10 -0400

    added slider items

--- assets/cocktellme.js
+++ assets/cocktellme.js
@@ -1,4 +1,5 @@
-cocktailURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic"
+//cocktailURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic"
+cocktailURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka"
 $.ajax({
 
   url: cocktailURL,
@@ -13,29 +14,29 @@ $.ajax({
     var newImg = $("<img>");
     newImg.attr("src", response.drinks[i].strDrinkThumb);
     newLi.append(newImg);
-    //$(".uk-slider-items").append(newLi);
+    $(".uk-slider-items").append(newLi);
   }
 });
 
-$('#google-button').on('click', function() {
+$('#google-button').on('click', function () {
   // Initialize with your OAuth.io app public key
   OAuth.initialize('SwtCfntFYnWRa2E5jhP30wnVEG8');
   // Use popup for OAuth
   OAuth.popup('google').then(google => {
     console.log(google);
     // Retrieves user data from oauth provider
-   console.log(google.me());
+    console.log(google.me());
 
-   if(google.provider === "google"){
-    console.log(google.provider)
-     window.location = './newuser.html';
-   }
+    if (google.provider === "google") {
+      console.log(google.provider)
+      window.location = './newuser.html';
+    }
 
   });
-  
+
 })
 
-$('#submit').on('click', function() {
+$('#submit').on('click', function () {
   window.location = './newuser.html';
 })
 
