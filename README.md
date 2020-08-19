0f8992a8b6e00dac2fed4246248aea6fbe33c9b2
Collin Lanier <collinlanier12@gmail.com>
2020-08-19 11:25:05 -0400

    Changes to readme. added functionality on results.html for your personality to your drink

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# cocktellme
-https://oksimone.github.io/cocktellme/
+# CockTellMe
+* CockTellME will pair users with cocktail drinks based on their personality traits.
+  
+## The User Story:
+> <ins>AS A</ins> user\
+> <ins>I WANT</ins> an application that pairs a drink with my personality traits\
+> <ins>SO THAT</ins> I know what to order at a restaurant or bar
+
+## Task To Be Completed:
+- [ ] Will be working on the layout and design. Will be implementing ID’s for team Back-End to call upon and grab values/text.
+- [ ] Adding script tags and new framework that will be used other than Bootstrap. (UIKit chosen for new framework)
+- [ ] Will be researching the API’s that will be used and adding functionality to web-page. 
+- [ ] Also will be setting the drinks of the cocktaildb api to the user based on the results from the personality test.
+
+## Framework Used:
+* UIKit [UIkit Link](https://getuikit.com/)
+  
+## API's used:
+* Tratify [Tratify API Link](https://www.traitify.com/api/)
+* Cocktail DB [Cocktail DB Link](https://www.thecocktaildb.com/api.php)
+* Google Sign [Google Sign in Link](https://developers.google.com/identity/sign-in/web/sign-in)
+* Oauth [Oauth Link](https://docs.oauth.io/)
+
+## Link to Website:
+[CocktellMe Wbesite!](https://oksimone.github.io/cocktellme/)
+
+## Demo:
+![Alt text for image](https://github.com/[githubusername]/[name-of-repo]/blob/master/[folder-name-of-images]/[name-of-pic (ex. something.png)])
+
--- assets/cocktellme.js
+++ assets/cocktellme.js
@@ -1,5 +1,12 @@
-cocktailURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic"
-cocktailURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka"
+cocktailURL = "https://www.thecocktaildb.com/api/json/v2/9973533/popular.php"
+
+var drinkWhiskey = "Whiskey";
+var drinkRum = "Spiced Rum";
+var drinkVod = "Vodka";
+var drinkTequila = "Tequila";
+var drinkScotch = "Scotch";
+var drinkLager = "Lager";
+var drinkCognac = "Cognac";
 
 $.ajax({
 
@@ -51,3 +58,215 @@ $('#submit').on('click', function () {
   }
 })
 
+function vodka() {
+  var cocktailURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + drinkVod;
+  $.ajax({
+
+    url: cocktailURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+    for (var i = 0; i < response.drinks.length; i++) {
+      var resultsLi = $("<li>")
+      var divCard = $("<div>").attr("class", "uk-card uk-card-default");
+      var caroDiv = $("<div>").attr("class", "uk-card-media-top");
+      var newImg = $("<img>").attr("src", response.drinks[i].strDrinkThumb);
+      console.log(newImg);
+      caroDiv.append(newImg);
+      divCard.append(caroDiv);
+      var divCardBody = $("<div>").attr("class", "uk-card-body");
+      var newH3 = $("<h3>").text(response.drinks[i].strDrink).css({ "color": "black" });
+      divCardBody.append(newH3);
+      divCard.append(divCardBody);
+      resultsLi.append(divCard);
+      $(".uk-slider-items").append(resultsLi);
+    }
+  });
+}
+function spicedRum() {
+  var cocktailURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + drinkRum;
+  $.ajax({
+
+    url: cocktailURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+    for (var i = 0; i < response.drinks.length; i++) {
+      var resultsLi = $("<li>")
+      var divCard = $("<div>").attr("class", "uk-card uk-card-default");
+      var caroDiv = $("<div>").attr("class", "uk-card-media-top");
+      var newImg = $("<img>").attr("src", response.drinks[i].strDrinkThumb);
+      console.log(newImg);
+      caroDiv.append(newImg);
+      divCard.append(caroDiv);
+      var divCardBody = $("<div>").attr("class", "uk-card-body");
+      var newH3 = $("<h3>").text(response.drinks[i].strDrink).css({ "color": "black" });
+      divCardBody.append(newH3);
+      divCard.append(divCardBody);
+      resultsLi.append(divCard);
+      $(".uk-slider-items").append(resultsLi);
+    }
+  });
+}
+function whiskey() {
+  var cocktailURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + drinkWhiskey;
+  $.ajax({
+    url: cocktailURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+    for (var i = 0; i < response.drinks.length; i++) {
+      var resultsLi = $("<li>")
+      var divCard = $("<div>").attr("class", "uk-card uk-card-default");
+      var caroDiv = $("<div>").attr("class", "uk-card-media-top");
+      var newImg = $("<img>").attr("src", response.drinks[i].strDrinkThumb);
+      console.log(newImg);
+      caroDiv.append(newImg);
+      divCard.append(caroDiv);
+      var divCardBody = $("<div>").attr("class", "uk-card-body");
+      var newH3 = $("<h3>").text(response.drinks[i].strDrink).css({ "color": "black" });
+      divCardBody.append(newH3);
+      divCard.append(divCardBody);
+      resultsLi.append(divCard);
+      $(".uk-slider-items").append(resultsLi);
+    }
+  });
+}
+function tequila() {
+  var cocktailURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + drinkTequila;
+  $.ajax({
+
+    url: cocktailURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+    for (var i = 0; i < response.drinks.length; i++) {
+      var resultsLi = $("<li>")
+      var divCard = $("<div>").attr("class", "uk-card uk-card-default");
+      var caroDiv = $("<div>").attr("class", "uk-card-media-top");
+      var newImg = $("<img>").attr("src", response.drinks[i].strDrinkThumb);
+      console.log(newImg);
+      caroDiv.append(newImg);
+      divCard.append(caroDiv);
+      var divCardBody = $("<div>").attr("class", "uk-card-body");
+      var newH3 = $("<h3>").text(response.drinks[i].strDrink).css({ "color": "black" });
+      divCardBody.append(newH3);
+      divCard.append(divCardBody);
+      resultsLi.append(divCard);
+      $(".uk-slider-items").append(resultsLi);
+    }
+  });
+}
+function scotch() {
+  var cocktailURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + drinkScotch;
+  $.ajax({
+
+    url: cocktailURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+    for (var i = 0; i < response.drinks.length; i++) {
+      var resultsLi = $("<li>")
+      var divCard = $("<div>").attr("class", "uk-card uk-card-default");
+      var caroDiv = $("<div>").attr("class", "uk-card-media-top");
+      var newImg = $("<img>").attr("src", response.drinks[i].strDrinkThumb);
+      console.log(newImg);
+      caroDiv.append(newImg);
+      divCard.append(caroDiv);
+      var divCardBody = $("<div>").attr("class", "uk-card-body");
+      var newH3 = $("<h3>").text(response.drinks[i].strDrink).css({ "color": "black" });
+      divCardBody.append(newH3);
+      divCard.append(divCardBody);
+      resultsLi.append(divCard);
+      $(".uk-slider-items").append(resultsLi);
+    }
+  });
+}
+function lager() {
+  var cocktailURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + drinkLager;
+  $.ajax({
+
+    url: cocktailURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+    for (var i = 0; i < response.drinks.length; i++) {
+      var resultsLi = $("<li>")
+      var divCard = $("<div>").attr("class", "uk-card uk-card-default");
+      var caroDiv = $("<div>").attr("class", "uk-card-media-top");
+      var newImg = $("<img>").attr("src", response.drinks[i].strDrinkThumb);
+      console.log(newImg);
+      caroDiv.append(newImg);
+      divCard.append(caroDiv);
+      var divCardBody = $("<div>").attr("class", "uk-card-body");
+      var newH3 = $("<h3>").text(response.drinks[i].strDrink).css({ "color": "black" });
+      divCardBody.append(newH3);
+      divCard.append(divCardBody);
+      resultsLi.append(divCard);
+      $(".uk-slider-items").append(resultsLi);
+    }
+  });
+}
+function cognac() {
+  var cocktailURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + drinkCognac;
+  $.ajax({
+
+    url: cocktailURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+    for (var i = 0; i < response.drinks.length; i++) {
+      var resultsLi = $("<li>")
+      var divCard = $("<div>").attr("class", "uk-card uk-card-default");
+      var caroDiv = $("<div>").attr("class", "uk-card-media-top");
+      var newImg = $("<img>").attr("src", response.drinks[i].strDrinkThumb);
+      console.log(newImg);
+      caroDiv.append(newImg);
+      divCard.append(caroDiv);
+      var divCardBody = $("<div>").attr("class", "uk-card-body");
+      var newH3 = $("<h3>").text(response.drinks[i].strDrink).css({ "color": "black" });
+      divCardBody.append(newH3);
+      divCard.append(divCardBody);
+      resultsLi.append(divCard);
+      $(".uk-slider-items").append(resultsLi);
+    }
+  });
+}
+
+
+var queryResults = "https://api.traitify.com/v1/assessments/" + "ddb69c62-70bc-4406-af2f-aeb47f329ea5" + "?data=blend,types,traits,career_matches"
+$.ajax({
+  url: queryResults,
+  beforeSend: function (xhr) {
+    xhr.setRequestHeader("Authorization", "Basic " + btoa("17330cfb2aff43ce8e99dd4889736e0b:x"));
+  },
+  type: 'GET',
+  dataType: 'json',
+  contentType: 'application/json',
+})
+  .then(function (response) {
+    console.log(response);
+    console.log(response.personality_blend.personality_type_1.name);
+    if (response.personality_blend.personality_type_1.name === "Action-Taker") {
+      whiskey();
+      $("#yourPersonalityCocktail").text("You are described as an Action-Taker. Action-Takers are hands-on doers! You excel at solitary, goal-oriented work and ‘getting the job done’. You’re also known for being handy, practical, applied, and ‘down to earth’.  The assessment has matched you with cocktails that contain " + drinkWhiskey + "!");
+    } else if (response.personality_blend.personality_type_1.name === "Inventor") {
+      spicedRum();
+      $("#yourPersonalityCocktail").text("You are described as an Inventor! Inventors are for being creative! Inventors excel when they are ‘lost in their work, completely engrossed in a project!.  The assessment has matched you with cocktails that contain " + drinkRum + "!");
+    } else if (response.personality_blend.personality_type_1.name === "Naturalist") {
+      vodka();
+      $("#yourPersonalityCocktail").text("You are described as being attached to the natural world, frequently working with plants and animals in their environment. Often nature lovers, they like to experience the world outdoors.  The assessment has matched you with cocktails that contain " + drinkVod + "!");
+    } else if (response.personality_blend.personality_type_1.name === "Visionary") {
+      tequila();
+      $("#yourPersonalityCocktail").text("You are described as a pioneer. You are a great convincer and like to work with others in a 'take charge' capacity. You love to take risks, make changes and set trends.  The assessment has matched you with cocktails that contain " + drinkTequila + "!");
+    } else if (response.personality_blend.personality_type_1.name === "Analyzer") {
+      scotch();
+      $("#yourPersonalityCocktail").text("You are described as an inquisitive person. They delve, study and look deeply for information. You have a keen eye and an experimental nature.  The assessment has matched you with cocktails that contain " + drinkScotch + "!");
+    } else if (response.personality_blend.personality_type_1.name === "Mentor") {
+      lager();
+      $("#yourPersonalityCocktail").text("You are described as a people-oriented person. You have great communication skills and are most fulfilled when assisting or working directly with others to improve a personal or societal situation.  The assessment has matched you with cocktails that contain " + drinkLager + "!");
+    } else if (response.personality_blend.personality_type_1.name === "Planner") {
+      cognac();
+      $("#yourPersonalityCocktail").text("You are described as a detailed person. You are methodical, precise, and detail-oriented. Planners create systems to follow, and enjoy working with data, detail, words and numbers.  The assessment has matched you with cocktails that contain " + drinkCognac + "!");
+    }
+  });
